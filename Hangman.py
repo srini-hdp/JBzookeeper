5426431e448c87679e6133add9d46a36ddb03c61
srini-hdp <avsrini@gmail.com>
2020-05-02 19:27:53 +1000

    Hangman with letter input

--- Hangman.py
+++ Hangman.py
@@ -1,13 +1,25 @@
 import random
 
 wordlist = ['python', 'java', 'kotlin', 'javascript']
-pick = random.choice(wordlist)
-
+pick = list(random.choice(wordlist))
+tries = 8
 print("H A N G M A N")
-print(f'Guess the word {pick[0:3]}{"-" * (len(pick) - 3)}: ')
-guess = input()
+guessed_list = list('-' * len(pick))
 
-if guess == pick:
-    print('You survived!')
-else:
-    print('You are hanged!')
+for n in range(tries):
+    print()
+    print("".join(guessed_list))
+    guess = input('Input a letter:')
+
+    if guess in pick:
+        i = 0
+        while i < len(pick):
+            if guess == pick[i]:
+                guessed_list[i] = pick[i]
+            i += 1
+    else:
+        print('No such letter in the word')
+
+print("""
+Thanks for playing!
+We'll see how well you did in the next stage""")
