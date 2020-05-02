1b242e3117ea19cba1c1f4d7b0cec409ffc3ad1c
srini-hdp <avsrini@gmail.com>
2020-05-02 19:28:32 +1000

    Hangman with 8 tries

--- Hangman.py
+++ Hangman.py
@@ -2,24 +2,32 @@ import random
 
 wordlist = ['python', 'java', 'kotlin', 'javascript']
 pick = list(random.choice(wordlist))
-tries = 8
+tries = 0
 print("H A N G M A N")
 guessed_list = list('-' * len(pick))
 
-for n in range(tries):
+while tries < 8:
     print()
     print("".join(guessed_list))
+    if guessed_list == pick:
+        print('You guessed the word!')
+        print('You survived!')
+        break
     guess = input('Input a letter:')
 
-    if guess in pick:
+    if guess in pick and guess not in guessed_list:
         i = 0
         while i < len(pick):
             if guess == pick[i]:
                 guessed_list[i] = pick[i]
             i += 1
+            continue
+    elif guess in guessed_list:
+        print('No improvements')
+        tries += 1
+        continue
     else:
         print('No such letter in the word')
-
-print("""
-Thanks for playing!
-We'll see how well you did in the next stage""")
+        tries += 1
+else:
+    print('You are hanged!')
